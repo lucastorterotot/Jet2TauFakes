65c6f9992f1d17336a410d6156a209a97deabb09
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2016-03-22 19:39:42 +0100

    Draft implementation of systematics management

--- interface/FakeFactor.h
+++ interface/FakeFactor.h
@@ -7,40 +7,83 @@
 #include <memory>
 #include <vector>
 #include <string>
+#include <map>
 
 
 class FakeFactor
 {
+    private:
+        // some aliases
+        using WrapperPtr = IFunctionWrapper*;
+        using WrapperItr = std::vector<WrapperPtr>::iterator;
 
     public:
-        FakeFactor() {};
+        FakeFactor() 
+        {
+            m_nodes.insert( std::make_pair("", std::vector<WrapperItr>()) );
+            m_indices.insert( std::make_pair("", std::vector<std::vector<size_t>>()) );
+            m_nodeInputs.insert( std::make_pair("", std::vector<std::vector<size_t>>()) )
+        };
         virtual ~FakeFactor();
 
-        double value(size_t size, const double* xs)
+        double value(size_t size, const double* xs, const std::string& sys="")
         {
             std::vector<double> vxs(xs, xs+size);
-            return value(vxs, m_nodes.size()-1); // evaluate the root
+            auto& sys_nodes = m_nodes.find(sys);
+            if(sys_nodes==m_nodes.end())
+            {
+                std::cout<<"[FakeFactor] ERROR: Non registered systematic "<<sys<<"\n";
+                return 1.;
+            }
+            auto& sys_indices = m_indices.find(sys);
+            auto& sys_inputs = m_inputs.find(sys);
+            return value(vxs, sys_nodes->second, sys_indice->second, sys_input->second, sys_nodes->second.size()-1); // evaluate the root
         }
-        double value(const std::vector<double>& xs)
+        double value(const std::vector<double>& xs, const std::string& sys="")
         {
-            return value(xs, m_nodes.size()-1); // evaluate the root
+            auto& sys_nodes = m_nodes.find(sys);
+            if(sys_nodes==m_nodes.end())
+            {
+                std::cout<<"[FakeFactor] ERROR: Non registered systematic "<<sys<<"\n";
+                return 1.;
+            }
+            auto& sys_indices = m_indices.find(sys);
+            auto& sys_inputs = m_inputs.find(sys);
+            return value(xs, sys_nodes->second, sys_indice->second, sys_input->second, sys_nodes->second.size()-1); // evaluate the root
         }
-        bool addNode(IFunctionWrapper* fct, size_t sonsSize, const size_t* sons, size_t varsSize, const size_t* vars)
+        bool addNode(WrapperPtr fct, size_t sonsSize, const size_t* sons, size_t varsSize, const size_t* vars, const std::string& sys="")
         {
             std::vector<size_t> vsons(sons, sons+sonsSize);
             std::vector<size_t> vvars(vars, vars+varsSize);
-            return addNode(fct, vsons, vvars);
+            return addNode(fct, vsons, vvars, sys);
         }
-        bool addNode(IFunctionWrapper*, const std::vector<size_t>&, const std::vector<size_t>&);
+        bool addNode(WrapperPtr, const std::vector<size_t>&, const std::vector<size_t>&, const std::string& sys="");
+        bool replaceNode(const std::string& name, WrapperPtr fct, size_t sonsSize, const size_t* sons, size_t varsSize, const size_t* vars, const std::string& sys="")
+        {
+            std::vector<size_t> vsons(sons, sons+sonsSize);
+            std::vector<size_t> vvars(vars, vars+varsSize);
+            return replaceNode(name, fct, vsons, vvars, sys);
+        }
+        bool replaceNode(const std::string&, WrapperPtr, const std::vector<size_t>&, const std::string& sys="");
+
+        void registerSystematic(const std::string& name)
+        {
+            // initialize with nominal objects
+            m_nodes.insert( std::make_pair(name, m_nodes[""]) );
+            m_indices.insert( std::make_pair(name, m_indices[""]) );
+            m_nodeInputs.insert( std::make_pair(name, m_nodeInputs[""]) );
+        }
 
     private:
-        double value(const std::vector<double>&, size_t);
+        double value(const std::vector<double>&, std::vector<WrapperItr>&, std::vector<std::vector<size_t>>&, std::vector<std::vector<size_t>>&, size_t);
 
         std::vector<std::string> m_inputs;
 
-        std::vector<IFunctionWrapper*> m_nodes; // FIXME: raw pointer. Should consider smart pointer
-        std::vector<std::vector<size_t>> m_indices;
-        std::vector<std::vector<size_t>> m_nodeInputs;
+        std::vector<WrapperPtr> m_wrappers; 
+
+        std::map<std::string, std::vector<WrapperItr>> m_nodes;
+        std::map<std::string, std::vector<std::vector<size_t>>> m_indices;
+        std::map<std::string, std::vector<std::vector<size_t>>> m_nodeInputs;
 
 
     //private:
--- src/FakeFactor.cpp
+++ src/FakeFactor.cpp
@@ -9,7 +9,7 @@
 FakeFactor::~FakeFactor()
 /*****************************************************************/
 {
-    for(auto fct : m_nodes)
+    for(auto fct : m_wrappers)
     {
         delete fct;
     }
@@ -17,35 +17,35 @@ FakeFactor::~FakeFactor()
 
 
 /*****************************************************************/
-double FakeFactor::value(const std::vector<double>& xs, size_t index)
+double FakeFactor::value(const std::vector<double>& xs, std::vector<WrapperItr>& nodes, std::vector<std::vector<size_t>>& indicesVec, std::vector<std::vector<size_t>>& inputsVec, size_t index)
 /*****************************************************************/
 {
     // TODO: add xs size check
-    const auto& indices = m_indices[index];
+    const auto& indices = indicesVec[index];
     if(indices.size()>1) // Node
     {
         std::vector<double> values;
         values.reserve(indices.size());
         for(size_t i : indices) values.push_back( value(xs,i) );
-        auto& node = m_nodes[index];
-        std::cout<<node->name()<<" = "<<node->value(values)<<"\n";
+        auto& node = *nodes[index];
+        //std::cout<<node->name()<<" = "<<node->value(values)<<"\n";
         return node->value(values);
     }
     else // Leaf
     {
-        auto& leaf = m_nodes[index];
-        const auto& inputs = m_nodeInputs[index];
+        auto& leaf = *nodes[index];
+        const auto& inputs = inputsVec[index];
         std::vector<double> xssubset;
         xssubset.reserve(inputs.size());
         for(size_t i : inputs) xssubset.push_back( xs[i] );
-        std::cout<<leaf->name()<<" = "<<leaf->value(xssubset)<<"\n";
+        //std::cout<<leaf->name()<<" = "<<leaf->value(xssubset)<<"\n";
         return leaf->value(xssubset);
     }
 }
 
 
 /*****************************************************************/
-bool FakeFactor::addNode(IFunctionWrapper* fct, const std::vector<size_t>& sons, const std::vector<size_t>& vars)
+bool FakeFactor::addNode(WrapperPtr fct, const std::vector<size_t>& sons, const std::vector<size_t>& vars, const std::string& sys)
 /*****************************************************************/
 {
     if(!fct)
@@ -53,17 +53,53 @@ bool FakeFactor::addNode(IFunctionWrapper* fct, const std::vector<size_t>& sons,
         std::cout<<"[FakeFactor] ERROR: Trying to add a nullptr\n";
         return false;
     }
+    auto& sys_nodes = m_nodes.find(sys);
+    if(sys_wrapper==m_nodes.end())
+    {
+        std::cout<<"[FakeFactor] ERROR: Non registered systematic "<<sys<<"\n";
+        return false;
+    }
+    auto& sys_indices = m_indices.find(sys);
+    auto& sys_inputs = m_nodeInputs.find(sys);
     // require that the son indices already exist (avoid cycles)
     for(size_t i : sons) 
     {
-        if(i>=m_nodes.size())
+        if(i>=sys_nodes->second.size())
         {
             std::cout<<"[FakeFactor] ERROR: Trying to add a node with non-existing sons\n";
             return false;
         }
     }
-    m_nodes.push_back(fct);
-    m_indices.push_back(sons);
-    m_nodeInputs.push_back(vars);
+    m_wrappers.push_back(fct);
+    sys_nodes->second.push_back(m_wrappers.end()-1);
+    sys_indices->second.push_back(sons);
+    sys_inputs->second.push_back(vars);
     return true;
+}
+
+
+/*****************************************************************/
+bool FakeFactor::replaceNode(const std::string& sys, const std::string& node, WrapperPtr fct, const std::vector<size_t>& sons)
+/*****************************************************************/
+{
+    if(!fct)
+    {
+        std::cout<<"[FakeFactor] ERROR: Trying to add a nullptr\n";
+        return false;
+    }
+    auto& sys_nodes = m_sysNodes.find(sys);
+    if(sys_wrapper==m_sysNodes.end())
+    {
+        std::cout<<"[FakeFactor] ERROR: Non registered systematic "<<sys<<"\n";
+        return false;
+    }
+    // require that the son indices already exist
+    for(size_t i : sons) 
+    {
+        if(i>=sys_nodes->second.size())
+        {
+            std::cout<<"[FakeFactor] ERROR: Trying to add a node with non-existing sons\n";
+            return false;
+        }
+    }
 }
