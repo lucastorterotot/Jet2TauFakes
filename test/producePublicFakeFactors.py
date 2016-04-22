c2bb475d715be507686e80e46a7820018136a126
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2016-04-22 13:49:59 +0200

    Add uncertainties to public fake factors

--- test/producePublicFakeFactors.py
+++ test/producePublicFakeFactors.py
@@ -1,16 +1,16 @@
-from HTTutilities.Jet2TauFakes.Utilities import Leaf, Node, fill, FakeFactor
+from HTTutilities.Jet2TauFakes.Utilities import Leaf, Node, fill, FakeFactor, replace_nodes
 import ROOT
 import os
 
 ## Meta-data
-version = '20160420'
-tag     = '0.1.1'
+version = '20160422'
+tag     = 'v0.1.2'
 
 # Individual fake factors
 ff_qcd_os = FakeFactor(vars=['tau_pt', 'tau_decay', 'mvis', 'mu_iso'])
 ff_qcd_ss = FakeFactor(vars=['tau_pt', 'tau_decay', 'mvis', 'mu_iso'])
 ff_w      = FakeFactor(vars=['tau_pt', 'tau_decay', 'mvis', 'mt'])
-ff_tt     = FakeFactor(vars=['tau_pt', 'tau_decay'])
+ff_tt     = FakeFactor(vars=['tau_pt', 'tau_decay', 'mvis'])
 # Combined fake factor
 ff_comb   = FakeFactor(vars=['tau_pt', 'tau_decay', 'mvis', 'mt', 'mu_iso'])
 
@@ -67,6 +67,79 @@ qcd_os = Node(
         )
     ]
 )
+# Define systematics
+qcd_ss_up = replace_nodes(
+    qcd_ss, 
+    {'ff_qcd_ss':
+     Node(
+         name='ff_qcd_ss_up',
+         formula='(1.+{sys_qcd_up})*{ff_qcd_ss}',
+         leaves=[
+             Leaf(
+                 name='sys_qcd_up',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_QCD_SS_MVis_Iso_up',
+                 vars=['mvis', 'mu_iso']
+             ),
+             qcd_ss.find('ff_qcd_ss')
+         ]
+     )
+    }
+)
+qcd_ss_down = replace_nodes(
+    qcd_ss, 
+    {'ff_qcd_ss':
+     Node(
+         name='ff_qcd_ss_down',
+         formula='(1.-{sys_qcd_down})*{ff_qcd_ss}',
+         leaves=[
+             Leaf(
+                 name='sys_qcd_down',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_QCD_SS_MVis_Iso_down',
+                 vars=['mvis', 'mu_iso']
+             ),
+             qcd_ss.find('ff_qcd_ss')
+         ]
+     )
+    }
+)
+qcd_os_up = replace_nodes(
+    qcd_os, 
+    {'ff_qcd_os':
+     Node(
+         name='ff_qcd_os_up',
+         formula='(1.+{sys_qcd_up})*{ff_qcd_os}',
+         leaves=[
+             Leaf(
+                 name='sys_qcd_up',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_QCD_MVis_Iso_SS2OS_up',
+                 vars=['mvis', 'mu_iso']
+             ),
+             qcd_os.find('ff_qcd_os')
+         ]
+     )
+    }
+)
+qcd_os_down = replace_nodes(
+    qcd_os, 
+    {'ff_qcd_os':
+     Node(
+         name='ff_qcd_os_down',
+         formula='(1.-{sys_qcd_down})*{ff_qcd_os}',
+         leaves=[
+             Leaf(
+                 name='sys_qcd_down',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_QCD_MVis_Iso_SS2OS_down',
+                 vars=['mvis', 'mu_iso']
+             ),
+             qcd_os.find('ff_qcd_os')
+         ]
+     )
+    }
+)
 
 ### W fake factors
 w = Node(
@@ -93,6 +166,44 @@ w = Node(
         )
     ]
 )
+# Define systematics
+w_up = replace_nodes(
+    w, 
+    {'ff_w':
+     Node(
+         name='ff_w_up',
+         formula='(1.+{sys_w_up})*{ff_w}',
+         leaves=[
+             Leaf(
+                 name='sys_w_up',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_W_MVis_MT_up',
+                 vars=['mvis', 'mt']
+             ),
+             w.find('ff_w')
+         ]
+     )
+    }
+)
+w_down = replace_nodes(
+    w, 
+    {'ff_w':
+     Node(
+         name='ff_w_down',
+         formula='(1.-{sys_w_down})*{ff_w}',
+         leaves=[
+             Leaf(
+                 name='sys_w_down',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_W_MVis_MT_down',
+                 vars=['mvis', 'mt']
+             ),
+             w.find('ff_w')
+         ]
+     )
+    }
+)
+
 
 ### TTbar fake factors
 tt = Leaf(
@@ -101,6 +212,29 @@ tt = Leaf(
     object='c_t_2d',
     vars=['tau_pt','tau_decay']
 )
+# Define systematics
+tt_up = Node(
+         name='ff_tt_up',
+         formula='(1.+{sys_tt})*{ff_tt}',
+         leaves=[
+             Leaf(
+                 name='sys_tt',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/sys_TT_J_nonclosure_mvis.root'.format(HOME=home,VERSION=version),
+                 object='sys',
+                 vars=['mvis']
+             ),
+             tt.find('ff_tt')
+         ]
+     )
+tt_down = Node(
+         name='ff_tt_down',
+         formula='(1.-{sys_tt})*{ff_tt}',
+         leaves=[
+             tt_up.find('sys_tt'),
+             tt.find('ff_tt')
+         ]
+     )
+
 
 
 ### Combined fake factors
@@ -139,15 +273,132 @@ comb = Node(
         ),
     ]
 )
-
+# Define systematics
+comb_qcd_up = replace_nodes(
+    comb, 
+    {'ff_qcd_os':
+     Node(
+         name='ff_qcd_os_up',
+         formula='(1.+{sys_qcd_up})*{ff_qcd_os}',
+         leaves=[
+             Leaf(
+                 name='sys_qcd_up',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_QCD_MVis_Iso_SS2OS_up',
+                 vars=['mvis', 'mu_iso']
+             ),
+             comb.find('ff_qcd_os')
+         ]
+     )
+    }
+)
+comb_qcd_down = replace_nodes(
+    comb, 
+    {'ff_qcd_os':
+     Node(
+         name='ff_qcd_os_down',
+         formula='(1.-{sys_qcd_down})*{ff_qcd_os}',
+         leaves=[
+             Leaf(
+                 name='sys_qcd_down',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_QCD_MVis_Iso_SS2OS_down',
+                 vars=['mvis', 'mu_iso']
+             ),
+             comb.find('ff_qcd_os')
+         ]
+     )
+    }
+)
+comb_w_up = replace_nodes(
+    comb, 
+    {'ff_w':
+     Node(
+         name='ff_w_up',
+         formula='(1.+{sys_w_up})*{ff_w}',
+         leaves=[
+             Leaf(
+                 name='sys_w_up',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_W_MVis_MT_up',
+                 vars=['mvis', 'mt']
+             ),
+             comb.find('ff_w')
+         ]
+     )
+    }
+)
+comb_w_down = replace_nodes(
+    comb, 
+    {'ff_w':
+     Node(
+         name='ff_w_down',
+         formula='(1.-{sys_w_down})*{ff_w}',
+         leaves=[
+             Leaf(
+                 name='sys_w_down',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/uncertainties_QCD_W.root'.format(HOME=home,VERSION=version),
+                 object='uncertainties_W_MVis_MT_down',
+                 vars=['mvis', 'mt']
+             ),
+             comb.find('ff_w')
+         ]
+     )
+    }
+)
+comb_tt_up = replace_nodes(
+    comb, 
+    {'ff_tt':
+     Node(
+         name='ff_tt_up',
+         formula='(1.+{sys_tt})*{ff_tt}',
+         leaves=[
+             Leaf(
+                 name='sys_tt',
+                 file='{HOME}/public/Htautau/FakeRate/{VERSION}/pieces/sys_TT_J_nonclosure_mvis.root'.format(HOME=home,VERSION=version),
+                 object='sys',
+                 vars=['mvis']
+             ),
+             comb.find('ff_tt')
+         ]
+     )
+    }
+)
+comb_tt_down = replace_nodes(
+    comb, 
+    {'ff_tt':
+     Node(
+         name='ff_tt_down',
+         formula='(1.-{sys_tt})*{ff_tt}',
+         leaves=[
+             comb_tt_up.find('sys_tt'),
+             comb.find('ff_tt')
+         ]
+     )
+    }
+)
 
 
 ## Fill fake factors
 fill(ff_qcd_ss, qcd_ss)
+fill(ff_qcd_ss, qcd_ss_up,   sys='ff_qcd_up')
+fill(ff_qcd_ss, qcd_ss_down, sys='ff_qcd_down')
 fill(ff_qcd_os, qcd_os)
+fill(ff_qcd_os, qcd_os_up,   sys='ff_qcd_up')
+fill(ff_qcd_os, qcd_os_down, sys='ff_qcd_down')
 fill(ff_w     , w)
+fill(ff_w, w_up,   sys='ff_w_up')
+fill(ff_w, w_down, sys='ff_w_down')
 fill(ff_tt    , tt)
+fill(ff_tt, tt_up,   sys='ff_tt_up')
+fill(ff_tt, tt_down, sys='ff_tt_down')
 fill(ff_comb  , comb)
+fill(ff_comb, comb_qcd_up,   sys='ff_qcd_up')
+fill(ff_comb, comb_qcd_down, sys='ff_qcd_down')
+fill(ff_comb, comb_w_up,     sys='ff_w_up')
+fill(ff_comb, comb_w_down,   sys='ff_w_down')
+fill(ff_comb, comb_tt_up,    sys='ff_tt_up')
+fill(ff_comb, comb_tt_down,  sys='ff_tt_down')
 
 
 file = ROOT.TFile.Open("{HOME}/public/Htautau/FakeRate/{VERSION}/fakeFactors_{VERSION}.root".format(HOME=home,VERSION=version), "recreate")
