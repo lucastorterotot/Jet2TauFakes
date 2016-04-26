4a1d11c3c1eb4c53aa2464ba1118880a6b9012bc
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2016-04-26 14:41:45 +0200

    Update public fake factor version

--- test/producePublicFakeFactors.py
+++ test/producePublicFakeFactors.py
@@ -3,8 +3,8 @@ import ROOT
 import os
 
 ## Meta-data
-version = '20160422'
-tag     = 'v0.1.2'
+version = '20160425'
+tag     = 'v0.1.3'
 
 # Individual fake factors
 ff_qcd_os = FakeFactor(vars=['tau_pt', 'tau_decay', 'mvis', 'mu_iso'])
