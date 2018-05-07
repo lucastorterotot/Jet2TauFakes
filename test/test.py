88f3388358d520ac2f7b2c5abc897c27ce089093
Markus Spanring <markusspanring@gmail.com>
2018-05-07 15:21:18 +0200

    changing test files for usage with new FF interface (get fractions as input)

--- test/test.py
+++ test/test.py
@@ -1,18 +1,18 @@
 import ROOT
 from array import array
 
-ff_fname="/afs/cern.ch/user/j/jbrandst/public/Htautau/FakeRate/2016/201610/mt/incl/fakeFactors_20161023.root"
+ff_fname="/afs/cern.ch/user/m/mspanrin/public/Htautau/FakeRate/et/incl/fakeFactors_20180412_tight.root"
 
 #Retrieve the fake factor
 ff_file = ROOT.TFile.Open(ff_fname)
 ff      = ff_file.Get("ff_comb")
 
 #Input names
-#Currently: tau_pt, tau_decay, njet, mvis, mt, muon_iso
+#Currently: tau_pt, tau_decay, njet, mvis, mt, muon_iso, frac_qcd, frac_w, frac_tt
 inputNames = ff.inputs() #this returns a ROOT.vector<string> object
 
 #Fill inputs
-inputs = [30,0,1,40,10,0.00]
+inputs = [30,0,1,40,10,0.00,0.4,0.3,0.2]
 
 ff_nom = ff.value( len(inputs),array('d',inputs) ) # nominal fake factor
 
@@ -30,8 +30,10 @@ stat_w_down = ff.value( len(inputs),array('d',inputs),"ff_w_stat_down" );
 stat_tt_up = ff.value( len(inputs),array('d',inputs),"ff_tt_stat_up" );
 stat_tt_down = ff.value( len(inputs),array('d',inputs),"ff_tt_stat_down" );
 
-print 'pt= {0}; dm= {1}; njet= {2}; mvis= {3}; mt= {4}; muiso= {5}'.format(inputs[0],inputs[1],inputs[2],inputs[3],inputs[4],inputs[5])
-print 'ff= {0:.6g}'.format(ff_nom)
+
+for name, val in zip(inputNames, inputs):
+	print "{0}= {1}, ".format(name,val),
+print '\nff= {0:.6g}'.format(ff_nom)
 print ' ----- Systematic uncertainties ----- '
 print 'Uncertainties on corrections:'
 print 'syst(tt) = {0:.6g}%, syst(w+dy) = {1:.6g}%, syst(qcd) = {2:.6g}%'.format( (syst_tt_up-ff_nom)/ff_nom*100, (syst_w_up-ff_nom)/ff_nom*100, (syst_qcd_up-ff_nom)/ff_nom*100  )
